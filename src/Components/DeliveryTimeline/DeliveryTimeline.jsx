15816184239251cc970790c4792ff97cb2127d4f
sly <sylvaincadoret@hotmail.com>
2024-07-09 16:56:56 +0200

    balise google added

--- src/Components/DeliveryTimeline/DeliveryTimeline.jsx
+++ src/Components/DeliveryTimeline/DeliveryTimeline.jsx
@@ -11,30 +11,39 @@ export default function DeliveryTimeline({ status }) {
   return (
     <div data-aos="flip-down" className={styles["delivery-timeline"]}>
       <div
-        className={`${styles["timeline-item"]} ${status === "pending" ? styles["active"] : ""}`}
+        className={`${styles["timeline-item"]} ${status === "paid" ? styles["active"] : ""}`}
       >
         <div className={styles["timeline-circle"]}>1</div>
         <div className={styles["timeline-content"]}>
+          <h4>Payé</h4>
+          <p> Commande payée </p>
+        </div>
+      </div>
+      <div
+        className={`${styles["timeline-item"]} ${status === "pending" ? styles["active"] : ""}`}
+      >
+        <div className={styles["timeline-circle"]}>2</div>
+        <div className={styles["timeline-content"]}>
           <h4>En attente</h4>
-          <p>Votre commande est en attente de traitement</p>
+          <p>En attente de traitement</p>
         </div>
       </div>
       <div
         className={`${styles["timeline-item"]} ${status === "shipped" ? styles["active"] : ""}`}
       >
-        <div className={styles["timeline-circle"]}>2</div>
+        <div className={styles["timeline-circle"]}>3</div>
         <div className={styles["timeline-content"]}>
           <h4>Expédiée</h4>
-          <p>Votre commande a été expédiée</p>
+          <p>Commande expédiée</p>
         </div>
       </div>
       <div
         className={`${styles["timeline-item"]} ${status === "delivered" ? styles["active"] : ""}`}
       >
-        <div className={styles["timeline-circle"]}>3</div>
+        <div className={styles["timeline-circle"]}>4</div>
         <div className={styles["timeline-content"]}>
           <h4>Livrée</h4>
-          <p>Votre commande a été livrée avec succès</p>
+          <p>Commande livrée</p>
         </div>
       </div>
     </div>
