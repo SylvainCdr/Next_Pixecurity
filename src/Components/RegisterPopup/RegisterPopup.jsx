7599b5b19044b2678f9bb90f4cc817e05604692b
sly <sylvaincadoret@hotmail.com>
2024-07-19 16:53:07 +0200

    RegisterPopup upgraded

--- src/Components/RegisterPopup/RegisterPopup.jsx
+++ src/Components/RegisterPopup/RegisterPopup.jsx
@@ -4,21 +4,23 @@ import { useGetUser } from '../useGetUser';
 
 export default function RegisterPopup() {
   const [show, setShow] = useState(false);
-
   const user = useGetUser();
 
-
-
-
   useEffect(() => {
-
     if (user) return; // If the user is already logged in, don't show the popup
+
+    const maxDisplays = 2;
+    const displayCount = parseInt(localStorage.getItem('popupDisplayCount')) || 0;
+
+    if (displayCount >= maxDisplays) return; // If the popup has been displayed 3 times, don't show it again
+
     const timer = setTimeout(() => {
       setShow(true);
-    },6000); // Adjust the timing as needed
+      localStorage.setItem('popupDisplayCount', displayCount + 1);
+    }, 6000); // Adjust the timing as needed
 
     return () => clearTimeout(timer); // Cleanup the timer on component unmount
-  }, []);
+  }, [user]);
 
   function closePopup() {
     setShow(false);
@@ -34,7 +36,7 @@ export default function RegisterPopup() {
     <div id="signup-popup" className={styles.popup}>
       <div className={styles["popup-content"]}>
         <span className={styles["close-btn"]} onClick={closePopup}>&times;</span>
-        <h2>Inscrivez-vous et obtenez <br /><span>-15%</span> </h2>
+        <h2>Inscrivez-vous et obtenez <br /><span>-15%</span></h2>
         <p>Rejoignez notre communauté et profitez d'offres exclusives et de réductions immédiates sur la Boutique en ligne.</p>
         <button onClick={redirectToSignup}>Inscrivez-vous maintenant</button>
       </div>
