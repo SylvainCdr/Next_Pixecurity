d9f20b8098f9493a3fe47ee0a8f8f46041394d9b
sly <sylvaincadoret@hotmail.com>
2024-07-25 17:14:42 +0200

    withAuth updated

--- src/utils/withAuth.js
+++ src/utils/withAuth.js
@@ -1,29 +1,21 @@
-// utils/withAuth.js
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useRouter } from "next/router";
 import { useGetUser } from "@/Components/useGetUser";
 
-export default function withAuth(WrappedComponent) {
-  return function AuthHOC(props) {
-    const [loading, setLoading] = useState(true);
+// fonction withAuth pour protéger les routes si le role stocké dans le localStorage n est pas admin
+
+export default function withAuth(Component) {
+  return function Auth() {
     const user = useGetUser();
     const router = useRouter();
     const role = user?.role;
 
     useEffect(() => {
-      if (loading) return; // Ne rien faire tant que le chargement n'est pas terminé
-
       if (role && role !== "admin") {
         router.push("/connexion");
       }
-    }, [role, router, loading]);
+    }, [role, router]);
 
-    // Si encore en train de charger les informations sur l'utilisateur, afficher un loader
-    if (loading) {
-      // Vous pouvez afficher un loader ici si nécessaire
-      return <div>Loading...</div>;
-    }
-
-    return <WrappedComponent {...props} />;
+    return <Component />;
   };
 }
