eb15d2093f7cb8c1f51c4792d403bb27baa0332d
sly <sylvaincadoret@hotmail.com>
2024-11-22 11:02:16 +0100

    actualites partners created

---
+++ src/pages/actualites-de-nos-partenaires.jsx
@@ -0,0 +1,3 @@
+import PartnersWhatsUp from "@/templates/PartnersWhatsUp/PartnersWhatsUp";
+
+export default PartnersWhatsUp;
---
+++ src/templates/PartnersWhatsUp/PartnersWhatsUp.jsx
@@ -0,0 +1,61 @@
+import styles from "./style.module.scss";
+import Link from "next/link";
+import Head from "next/head";
+
+export default function PartnersWhatsUp() {
+  return (
+    <>
+      <Head>
+        <title>Actualités de Nos Partenaires</title>
+        <meta
+          name="description"
+          content="Découvrez les actualités et annonces importantes de nos partenaires de confiance. Leurs innovations et leurs solutions avancées renforcent notre mission : vous offrir une sécurité de pointe. Restez à jour avec les leaders du secteur."
+        />
+        <meta property="og:title" content="Actualités de Nos Partenaires" />
+        <meta
+          property="og:description"
+          content="Découvrez les actualités et annonces importantes de nos partenaires de confiance. Leurs innovations et leurs solutions avancées renforcent notre mission : vous offrir une sécurité de pointe. Restez à jour avec les leaders du secteur."
+        />
+
+        <meta
+          name="keywords"
+          content="actualités, partenaires, sécurité, sûreté, videoprotection, caméra, contrôle d'accès, innovation, solutions, leaders, i-pro, vivotek, milestone, bosch, vuwall, zyxel"
+        />
+      </Head>
+
+      <div className={styles.partnersWhatsUpContainer}>
+        <div className={styles.partnersWhatsUpHeader}>
+          <div className={styles.headerLeft}>
+            <h1>Actualités de Nos Partenaires</h1>
+            <p>
+              Découvrez les actualités et annonces importantes de nos
+              partenaires de confiance. Leurs innovations et leurs solutions
+              avancées renforcent notre mission : vous offrir une sécurité de
+              pointe. Restez à jour avec les leaders du secteur.
+            </p>
+          </div>
+          <div className={styles.headerRight}></div>
+        </div>
+
+        <iframe
+          width="1440"
+          height="3000"
+          src="https://rss.app/embed/v1/wall/_uW33og8IouUH8pL5"
+          frameborder="0"
+        ></iframe>
+
+        {/* // Lien vers la boutique en ligne et nous contacter */}
+        <div className={styles.links}>
+          <button>
+            <Link href="https://www.google.com">Visiter la Boutique</Link>
+          </button>
+          <button>
+            <Link href="https://www.google.com">
+              Un Projet ? Contactez-nous
+            </Link>
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
