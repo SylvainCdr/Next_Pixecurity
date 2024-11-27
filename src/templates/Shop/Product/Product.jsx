8e561571c805393dd78e6828a4f0f22dc48799f9
sly <sylvaincadoret@hotmail.com>
2024-11-27 10:16:16 +0100

    product upgraded (cta contact)

--- src/templates/Shop/Product/Product.jsx
+++ src/templates/Shop/Product/Product.jsx
@@ -29,7 +29,6 @@ export default function Product({ product, id, suggestions }) {
 
   const [searchResults, setSearchResults] = useState([]);
 
-
   const labelsMapping = {
     dimensions: "Dimensions",
     poids: "Poids",
@@ -136,7 +135,7 @@ export default function Product({ product, id, suggestions }) {
 
   const handleSearchResults = (results) => {
     setSearchResults(results);
-  }
+  };
 
   useEffect(() => {
     const jsonLdData = {
@@ -187,209 +186,214 @@ export default function Product({ product, id, suggestions }) {
         <meta property="og:image" content={product.image} />
 
         <script
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{
-          __html: JSON.stringify({
-            "@context": "https://schema.org/",
-            "@type": "Product",
-            name: product.name,
-            description: product.description,
-            image: product.image.startsWith("http")
-              ? product.image
-              : `${BASE_URL}${product.image}`,
-            url: `https://pixecurity.com/boutique/produit/${product._id}`,
-            brand: {
-              "@type": "Brand",
-              name: product.brand,
-            },
-            offers: {
-              "@type": "Offer",
-              priceCurrency: "EUR",
-              price: product.price.toFixed(2),
-              priceValidUntil: "2024-12-31",
-              itemCondition: "https://schema.org/NewCondition",
-              availability: "https://schema.org/InStock",
-            },
-            mpn: product.ref,
-
-
-          }),
-        }}
-      />
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org/",
+              "@type": "Product",
+              name: product.name,
+              description: product.description,
+              image: product.image.startsWith("http")
+                ? product.image
+                : `${BASE_URL}${product.image}`,
+              url: `https://pixecurity.com/boutique/produit/${product._id}`,
+              brand: {
+                "@type": "Brand",
+                name: product.brand,
+              },
+              offers: {
+                "@type": "Offer",
+                priceCurrency: "EUR",
+                price: product.price.toFixed(2),
+                priceValidUntil: "2024-12-31",
+                itemCondition: "https://schema.org/NewCondition",
+                availability: "https://schema.org/InStock",
+              },
+              mpn: product.ref,
+            }),
+          }}
+        />
       </Head>
 
       <RegisterPopup />
       <ShopNav />
       <ShopSearch onSearchResults={handleSearchResults} />
       {searchResults.length === 0 && (
+        <div className={styles["product-page"]}>
+          <div className={styles["product-section1"]}>
+            <div data-aos="zoom-in-right" className={styles["product-img"]}>
+              {product.pourcentageDiscount !== 0 && (
+                <p className={styles["discount-badge"]}>
+                  -{product.pourcentageDiscount}%
+                </p>
+              )}
 
-      <div className={styles["product-page"]}>
-
-
-        <div className={styles["product-section1"]}>
-          <div data-aos="zoom-in-right" className={styles["product-img"]}>
-            {product.pourcentageDiscount !== 0 && (
-              <p className={styles["discount-badge"]}>
-                -{product.pourcentageDiscount}%
-              </p>
-            )}
-
-            <img
-              src={
-                product.image && product.image.startsWith("http")
-                  ? product.image
-                  : `${BASE_URL}${product.image}`
-              }
-              alt={product.name}
-              loading="lazy"
-            />
-            <p
-              className={styles.like}
-              data-aos="zoom-in-left"
-              onClick={handleToggleFavoritesClick}
-              style={{ cursor: "pointer" }}
-            >
-              <i
-                className="fa-solid fa-heart"
-                style={{ color: isInFavorites ? "#ed3f3f" : "inherit" }}
-              ></i>
-            </p>
-          </div>
-          <div className={styles["product-description"]}>
-            <h1>{product.name}</h1>
-
-            {brandLogo ? (
               <img
-                src={brandLogo.logo}
-                alt={product.brand}
-                className={styles["brand-logo"]}
+                src={
+                  product.image && product.image.startsWith("http")
+                    ? product.image
+                    : `${BASE_URL}${product.image}`
+                }
+                alt={product.name}
                 loading="lazy"
               />
-            ) : (
-              <p className={styles["brand-logo"]}>{product.brand}</p>
-            )}
-
-            <p className={styles.presentation}>{product.presentation}</p>
-
-            {product.category === "Logiciels" ? (
-              <p className={styles.stock}>
-                <i className="fa-solid fa-check"></i> En stock
+              <p
+                className={styles.like}
+                data-aos="zoom-in-left"
+                onClick={handleToggleFavoritesClick}
+                style={{ cursor: "pointer" }}
+              >
+                <i
+                  className="fa-solid fa-heart"
+                  style={{ color: isInFavorites ? "#ed3f3f" : "inherit" }}
+                ></i>
               </p>
-            ) : (
-              <p className={styles.stock}>
-                <i className="fa-solid fa-check"></i> Disponible sur commande
-              </p>
-            )}
+            </div>
+            <div className={styles["product-description"]}>
+              <h1>{product.name}</h1>
 
-            <div className={styles.details}>
-              <p className={styles.securePayment}>
-                <i className="fa-solid fa-lock"></i> Paiement sécurisé
-              </p>
-              {product.category === "Logiciels" ? (
-                <p>
-                  <i className="fa-solid fa-envelope"></i> Activation sous 48 à
-                  72 heures
-                </p>
-              ) : (
-                <p>
-                  <i className="fa-solid fa-truck-fast"></i> Livraison sous 2 à
-                  3 semaines via DHL
-                </p>
-              )}
-              {product.category !== "Logiciels" && (
+              {brandLogo ? (
                 <img
-                  src="https://www.dhl.com/content/dam/dhl/global/core/images/logos/dhl-logo.svg"
-                  alt="DHL-logo"
+                  src={brandLogo.logo}
+                  alt={product.brand}
+                  className={styles["brand-logo"]}
+                  loading="lazy"
                 />
-              )}
-            </div>
-
-            <div className={styles["price-addToCart"]}>
-              <div className={styles.price}>
-                <DiscountedPrice product={product} />
-              </div>
-              <div className={styles.quantity}>
-                <button
-                  onClick={() => setQuantity(quantity - 1)}
-                  disabled={quantity === 1}
-                  className={styles["qty-input"]}
-                >
-                  -
-                </button>
-                <p>{quantity}</p>
-                <button
-                  onClick={() => setQuantity(quantity + 1)}
-                  className={styles["qty-input"]}
-                >
-                  +
-                </button>
-              </div>
-
-              <button onClick={handleAddToCartClick}>Ajouter au panier</button>
-            </div>
-            <p className={styles.ref}>Référence : {product.ref}</p>
-          </div>
-        </div>
-
-        <div className={styles["product-section2"]}>
-          <div className={styles["product-details"]}>
-            <h3>Détails du produit</h3>
-            <p>{product.description}</p>
-            <table>
-              <tbody>
-                {product.details &&
-                  Object.keys(product.details).map(
-                    (key) =>
-                      product.details[key] !== "" && (
-                        <tr key={key}>
-                          <td>{labelsMapping[key] || key}</td>
-                          <td>{product.details[key]}</td>
-                        </tr>
-                      )
-                  )}
-              </tbody>
-            </table>
-
-            {product.pdf &&
-              (product.category === "Logiciels" ? (
-                <Link
-                  href={product.pdf}
-                  target="_blank"
-                  className={styles["pdf-link"]}
-                >
-                  En savoir plus{" "}
-                  <i className="fa-solid fa-arrow-up-right-from-square"></i>
-                </Link>
               ) : (
-                <Link
-                  href={product.pdf}
-                  download
-                  target="_blank"
-                  className={styles["pdf-link"]}
-                >
-                  Fiche technique <i className="fa-solid fa-file-pdf"></i>
+                <p className={styles["brand-logo"]}>{product.brand}</p>
+              )}
+
+              <p className={styles.presentation}>{product.presentation}</p>
+
+              {product.category === "Logiciels" ? (
+                <p className={styles.stock}>
+                  <i className="fa-solid fa-check"></i> En stock
+                </p>
+              ) : (
+                <p className={styles.stock}>
+                  <i className="fa-solid fa-check"></i> Disponible sur commande
+                </p>
+              )}
+
+              <p className={styles.inquiry}>
+                <i className="fa-solid fa-info-circle"></i> Pour vos besoins spécifiques, vos commandes en quantité ou vos devis groupés, {" "} 
+                <Link href="/contact" className={styles.contactLink}>
+                  contactez-nous
                 </Link>
-              ))}
+                . 
+              </p>
+
+              <div className={styles.details}>
+                <p className={styles.securePayment}>
+                  <i className="fa-solid fa-lock"></i> Paiement sécurisé
+                </p>
+                {product.category === "Logiciels" ? (
+                  <p>
+                    <i className="fa-solid fa-envelope"></i> Activation sous 48
+                    à 72 heures
+                  </p>
+                ) : (
+                  <p>
+                    <i className="fa-solid fa-truck-fast"></i> Livraison sous 2
+                    à 3 semaines via DHL
+                  </p>
+                )}
+                {product.category !== "Logiciels" && (
+                  <img
+                    src="https://www.dhl.com/content/dam/dhl/global/core/images/logos/dhl-logo.svg"
+                    alt="DHL-logo"
+                  />
+                )}
+              </div>
+
+              <div className={styles["price-addToCart"]}>
+                <div className={styles.price}>
+                  <DiscountedPrice product={product} />
+                </div>
+                <div className={styles.quantity}>
+                  <button
+                    onClick={() => setQuantity(quantity - 1)}
+                    disabled={quantity === 1}
+                    className={styles["qty-input"]}
+                  >
+                    -
+                  </button>
+                  <p>{quantity}</p>
+                  <button
+                    onClick={() => setQuantity(quantity + 1)}
+                    className={styles["qty-input"]}
+                  >
+                    +
+                  </button>
+                </div>
+
+                <button onClick={handleAddToCartClick}>
+                  Ajouter au panier
+                </button>
+              </div>
+              <p className={styles.ref}>Référence : {product.ref}</p>
+            </div>
           </div>
 
-          <div className={styles["product-suggestions"]}>
-            <h3>Produits similaires</h3>
-            <div className={styles["suggestions-grid"]}>
-              {suggestions.map((item) => (
-                <ProductCard
-                  key={item._id}
-                  product={item}
-                  isInFavorites={isInFavorites}
-                  addToFavorites={addToFavorites}
-                  removeFromFavorites={removeFromFavorites}
-                  checkFavorite={checkFavorite}
-                  getFavorites={getFavorites}
-                />
-              ))}
+          <div className={styles["product-section2"]}>
+            <div className={styles["product-details"]}>
+              <h3>Détails du produit</h3>
+              <p>{product.description}</p>
+              <table>
+                <tbody>
+                  {product.details &&
+                    Object.keys(product.details).map(
+                      (key) =>
+                        product.details[key] !== "" && (
+                          <tr key={key}>
+                            <td>{labelsMapping[key] || key}</td>
+                            <td>{product.details[key]}</td>
+                          </tr>
+                        )
+                    )}
+                </tbody>
+              </table>
+
+              {product.pdf &&
+                (product.category === "Logiciels" ? (
+                  <Link
+                    href={product.pdf}
+                    target="_blank"
+                    className={styles["pdf-link"]}
+                  >
+                    En savoir plus{" "}
+                    <i className="fa-solid fa-arrow-up-right-from-square"></i>
+                  </Link>
+                ) : (
+                  <Link
+                    href={product.pdf}
+                    download
+                    target="_blank"
+                    className={styles["pdf-link"]}
+                  >
+                    Fiche technique <i className="fa-solid fa-file-pdf"></i>
+                  </Link>
+                ))}
+            </div>
+
+            <div className={styles["product-suggestions"]}>
+              <h3>Produits similaires</h3>
+              <div className={styles["suggestions-grid"]}>
+                {suggestions.map((item) => (
+                  <ProductCard
+                    key={item._id}
+                    product={item}
+                    isInFavorites={isInFavorites}
+                    addToFavorites={addToFavorites}
+                    removeFromFavorites={removeFromFavorites}
+                    checkFavorite={checkFavorite}
+                    getFavorites={getFavorites}
+                  />
+                ))}
+              </div>
             </div>
           </div>
         </div>
-      </div>
       )}
     </div>
   );
