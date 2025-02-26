e25b4783e3418d8627605afc57c69e1d2f194225
sly <sylvaincadoret@hotmail.com>
2025-02-26 16:47:49 +0100

    PAGES routes fixed v1

--- src/url.js
+++ src/url.js
@@ -1,3 +1,3 @@
-// export const BASE_URL =  process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3001";
+export const BASE_URL =  process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3001";
 
-export const BASE_URL = "http://localhost:3001";
+// export const BASE_URL = "http://localhost:3001";
