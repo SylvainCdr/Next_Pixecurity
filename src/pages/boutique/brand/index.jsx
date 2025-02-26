98096144b99394dcc9d8600bffbc3f5a6fabfd2c
sly <sylvaincadoret@hotmail.com>
2025-02-26 15:30:49 +0100

    push to Netlify

--- src/pages/boutique/brand/index.jsx
+++ src/pages/boutique/brand/index.jsx
@@ -1,50 +1,50 @@
-// import { getProductsByCatSubCat } from "@/api/products";
-// import Products from "@/templates/Shop/Products/Products";
-// import {
-//   getProductsFiltered,
-//   getFiltersFromProducts,
-// } from "@/utils/getProductsFiltered";
+import { getProductsByCatSubCat } from "@/api/products";
+import Products from "@/templates/Shop/Products/Products";
+import {
+  getProductsFiltered,
+  getFiltersFromProducts,
+} from "@/utils/getProductsFiltered";
 
-// export async function getServerSideProps({ params, query }) {
-//   const brand = params.brand || "";
-//   const category = params.category || "";
-//   const subcategory = params.subcategory || "";
-//   const userId = query.userId || "";
+export async function getServerSideProps({ params, query }) {
+  const brand = params.brand || "";
+  const category = params.category || "";
+  const subcategory = params.subcategory || "";
+  const userId = query.userId || "";
 
-//   if (!category) {
-//     return { notFound: true };
-//   }
+  if (!category) {
+    return { notFound: true };
+  }
 
-//   const products = await getProductsByCatSubCat({
-//     brand,
-//     category,
-//     subcategory,
-//     userId,
-//   });
-//   const productsFiltered = getProductsFiltered(products, query);
-//   const filters = getFiltersFromProducts(productsFiltered);
+  const products = await getProductsByCatSubCat({
+    brand,
+    category,
+    subcategory,
+    userId,
+  });
+  const productsFiltered = getProductsFiltered(products, query);
+  const filters = getFiltersFromProducts(productsFiltered);
 
-//   return {
-//     props: {
-//       products: productsFiltered,
-//       brand,
-//       category,
-//       subcategory,
-//       filters,
-//     },
-//   };
-// }
+  return {
+    props: {
+      products: productsFiltered,
+      brand,
+      category,
+      subcategory,
+      filters,
+    },
+  };
+}
 
-// const Page = ({ products, brand, category, subcategory, filters }) => {
-//   return (
-//     <Products
-//       products={products}
-//       brand={brand}
-//       category={category}
-//       subcategory={subcategory}
-//       filters={filters}
-//     />
-//   );
-// };
+const Page = ({ products, brand, category, subcategory, filters }) => {
+  return (
+    <Products
+      products={products}
+      brand={brand}
+      category={category}
+      subcategory={subcategory}
+      filters={filters}
+    />
+  );
+};
 
-// export default Page;
+export default Page;
